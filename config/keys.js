860ca7b67b7abcc747315fd4bd38718151546ce9
raihan-js <araihansikder@gmail.com>
2022-08-25 23:33:43 +0600

    reconf uri

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://raihan-js:JIJLVM1mOMbRTDuh@cluster0.sjvnqli.mongodb.net/devstorm?retryWrites=true&w=majority',
+    mongoURI: '',
     secretOrKey: 'secret',
 }
