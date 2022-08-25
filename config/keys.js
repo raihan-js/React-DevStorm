65aff34010e57030abd274f7b0692e47edda80e9
raihan-js <araihansikder@gmail.com>
2022-08-25 23:37:10 +0600

    gitignore reconf

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: '',
+    mongoURI: 'mongodb+srv://raihan-js:JIJLVM1mOMbRTDuh@cluster0.sjvnqli.mongodb.net/devstorm?retryWrites=true&w=majority',
     secretOrKey: 'secret',
 }
